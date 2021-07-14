e07dbb531e685ffee8f9ecdbeb27d55977f06f9d
Quan Do <minhquan9607@gmail.com>
2021-07-14 11:35:30 +0300

    Initial commit of anecdote

---
+++ part1/anecdotes/src/Anecdote.js
@@ -0,0 +1,11 @@
+const Anecdote = ({text, votes}) => {
+    return (
+        <div>
+            <p>{text}</p>
+            <p>has {votes} votes</p>
+        </div>
+        
+    )
+}
+
+export default Anecdote;
